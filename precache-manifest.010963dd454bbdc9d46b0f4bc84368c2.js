2611b196d2ec1f0e4f358d5a9bbc2bbdd96f4a8d
Finbarr Ekomobong <ekfinbarr@gmail.com>
2020-09-15 03:29:55 +0100

    Initialize gh-pages

---
+++ precache-manifest.010963dd454bbdc9d46b0f4bc84368c2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91aec5cc0e99b2323f0",
+    "url": "/admin-two-vue-bulma-dashboard/css/app.60d5f335.css"
+  },
+  {
+    "revision": "8044447075d66d34a4655d0d6d654bb2",
+    "url": "/admin-two-vue-bulma-dashboard/data-sources/clients.json"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/admin-two-vue-bulma-dashboard/favicon.png"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/admin-two-vue-bulma-dashboard/img/jblogo-icon.9ed33b87.png"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "/admin-two-vue-bulma-dashboard/img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "89221892b2461e1abd455050a038b85d",
+    "url": "/admin-two-vue-bulma-dashboard/index.html"
+  },
+  {
+    "revision": "d91aec5cc0e99b2323f0",
+    "url": "/admin-two-vue-bulma-dashboard/js/app.3f35d16c.js"
+  },
+  {
+    "revision": "97df9267d9502722af4d",
+    "url": "/admin-two-vue-bulma-dashboard/js/chunk-vendors.08fece91.js"
+  },
+  {
+    "revision": "035848e5cc3a1ae2ab42",
+    "url": "/admin-two-vue-bulma-dashboard/js/forms.9cae70bd.js"
+  },
+  {
+    "revision": "c909d0c5f20533ea9bd2",
+    "url": "/admin-two-vue-bulma-dashboard/js/profile.fc581b0b.js"
+  },
+  {
+    "revision": "018718c75fc390a2d61a",
+    "url": "/admin-two-vue-bulma-dashboard/js/tables.2a2d1355.js"
+  },
+  {
+    "revision": "386d14efc71884e3ad7ccecba4811df6",
+    "url": "/admin-two-vue-bulma-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/admin-two-vue-bulma-dashboard/robots.txt"
+  }
+]);
